fece8ec9145530584d7946c8202d3857d006684a
cledson-leite <csbetsonline@gmail.com>
2021-12-11 13:31:45 -0300

    refactor: make sut creation to a factory helper method

--- src/presentation/search_cep_controller.spec.ts
+++ src/presentation/search_cep_controller.spec.ts
@@ -1,10 +1,14 @@
 import { MissingCep } from '../utils/errors/missing_cep_error'
 import SearchCepController from './search_cep_controller'
 
+const makeSut = (): SearchCepController => {
+  return new SearchCepController()
+}
+
 describe('SearchCepController', () => {
   it('Should return 400 if no cep is provided', () => {
     //produz os dados do teste
-    const sut = new SearchCepController()
+    const sut = makeSut()
     const cep: number = 12345678
 
     //operacionar esses dados
