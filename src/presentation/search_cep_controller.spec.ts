c2b702e40efd3d0396294f722fde2933f8f7f75f
cledson-leite <csbetsonline@gmail.com>
2021-12-11 14:18:51 -0300

    test: ensure return 400 if an invalid cep is provided

--- src/presentation/search_cep_controller.spec.ts
+++ src/presentation/search_cep_controller.spec.ts
@@ -1,15 +1,29 @@
-import { MissingCep } from '../utils/errors/missing_cep_error'
+import { InvalidCep, MissingCep } from '../utils/errors/missing_cep_error'
 import SearchCepController from './search_cep_controller'
+import ICEPValidator from './validator/i_cep_validator'
 
-const makeSut = (): SearchCepController => {
-  return new SearchCepController()
+class CEPValidatorStub implements ICEPValidator{
+  isValid(cep: String): boolean {
+    return true
+  }
+}
+
+interface SutTypes {
+  sut: SearchCepController,
+  validator: ICEPValidator
+}
+
+const makeSut = (): SutTypes => {
+  const validator = new CEPValidatorStub()
+  const sut = new SearchCepController(validator)
+  return {sut, validator}
 }
 
 describe('SearchCepController', () => {
   it('Should return 400 if no cep is provided', () => {
     //produz os dados do teste
-    const sut = makeSut()
-    const cep: number = 12345678
+    const {sut} = makeSut()
+    const cep: String = ''
 
     //operacionar esses dados
     const result = sut.search(cep)
@@ -17,6 +31,20 @@ describe('SearchCepController', () => {
     //verificar resultado esperado
     expect(result.statusCode).toBe(400)
     expect(result.data).toEqual(new MissingCep())
+
+  })
+  it('Should return 400 if an invalid cep is provided', () => {
+    //produz os dados do teste
+    const {sut, validator} = makeSut()
+    const cep: String = 'invalidCEP'
+    jest.spyOn(validator, 'isValid').mockReturnValueOnce(false)
+
+    //operacionar esses dados
+    const result = sut.search(cep)
+
+    //verificar resultado esperado
+    expect(result.statusCode).toBe(400)
+    expect(result.data).toEqual(new InvalidCep())
 
   })
 })
