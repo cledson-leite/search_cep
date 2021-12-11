2b749beb9443eb98d6666aff0df4829ffdb1dd09
cledson-leite <csbetsonline@gmail.com>
2021-12-11 12:23:39 -0300

    test: ensure return a error if no cep is provided

--- src/presentation/search_cep.spec.ts
+++ src/presentation/search_cep.spec.ts
@@ -5,12 +5,13 @@ describe('SearchCep', () => {
     //produz os dados do teste
     const sut = new SearchCep()
     const cep: number = 12345678
-    
+
     //operacionar esses dados
     const result = sut.search(cep)
     
     //verificar resultado esperado
     expect(result.statusCode).toBe(400)
+    expect(result.data).toEqual(new Error('Missing cep'))
     
   })
 })
