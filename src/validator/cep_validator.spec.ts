26b6f99441a3525f9d5cff6fa34bce38d22c9abd
cledson-leite <csbetsonline@gmail.com>
2021-12-11 16:46:33 -0300

    test: ensure return true if cep is valid

--- src/validator/cep_validator.spec.ts
+++ src/validator/cep_validator.spec.ts
@@ -14,4 +14,17 @@ describe('CepValidator', () => {
     expect(isValid).toBeFalsy()
     
   })
+  it('Should return true if cep is valid', () => {
+    //produz os dados do teste
+    const sut = new CepValidator()
+    const cep = '12345678'
+    
+    //operacionar esses dados
+    const isValid = sut.isValid(cep)
+    
+
+    //verificar resultado esperado
+    expect(isValid).toBeTruthy()
+    
+  })
 })
